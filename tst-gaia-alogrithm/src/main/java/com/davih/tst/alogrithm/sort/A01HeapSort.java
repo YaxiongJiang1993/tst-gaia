f41a500bbb36dc257c8d9e873958e0f1b91c80ee
David Jiang <david.jiang@bitkernel.tech>
2025-11-03 17:31:08 +0800

    test

---
+++ tst-gaia-alogrithm/src/main/java/com/davih/tst/alogrithm/sort/A01HeapSort.java
@@ -0,0 +1,11 @@
+package com.davih.tst.alogrithm.sort;
+
+public class A01HeapSort {
+
+
+    public void headFirst(int[] nums, int k, int len){
+        nums[0]=nums[k];
+
+    }
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
--- tst-gaia-alogrithm/src/main/java/com/davih/tst/alogrithm/t2025/A23ReverseKGroupNode.java
+++ tst-gaia-alogrithm/src/main/java/com/davih/tst/alogrithm/t2025/A23ReverseKGroupNode.java
@@ -2,9 +2,46 @@ package com.davih.tst.alogrithm.t2025;
 
 public class A23ReverseKGroupNode {
 
-    public ListNode reverseKGroup(ListNode head, int k){
+    public ListNode reverseKGroup(ListNode head, int k) {
 
-        return head;
+        ListNode h = new ListNode(-1);
+        h.next = head;
+
+        ListNode p1 = h, p = h;
+        int count = 0;
+        while (p.next != null) {
+            p = p.next;
+            count++;
+            if (count == k) {
+                count = 0;
+                p = reverse(p1, k);
+                p1 = p;
+            }
+        }
+
+        return h.next;
+    }
+
+    public ListNode reverse(ListNode head, int k) {
+        ListNode p = head.next;
+        ListNode p2 = p;
+        head.next = null;
+        int count = 0;
+        while (p != null) {
+            ListNode t = p.next;
+            p.next = head.next;
+            head.next = p;
+            p = t;
+            count++;
+            if (count == k) {
+                break;
+            }
+        }
+
+        p2.next = p;
+
+        return p2;
+
     }
 
     public class ListNode {
